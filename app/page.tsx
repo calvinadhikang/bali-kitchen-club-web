3c71649b915d65ae238a367159dbca9d09501e70
Calvin Adhikang <calvinadhikang@gmail.com>
2024-03-25 23:25:13 +0700

    add qr generator

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+'use client'
+
+import QRCode from "qrcode.react";
+import { useState } from "react";
 
 export default function Page() {
+
+    const [text, setText] = useState("")
+
     return (
         <>
             <h1>Welcome, to Bali Kitchen Club</h1>
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} className="border" />
+            <div className="w-screen flex items-center justify-center">
+                {text && <QRCode value={text} />}
+            </div>
         </>
     );
 }

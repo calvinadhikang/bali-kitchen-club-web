9dd7e39aff19189c038cd4b4e0362b0ff305360b
calvinadhikang <calvinadhikang@gmail.com>
2024-07-28 01:37:01 +0700

    remove default keyword

--- src/app/api/staff/add/route.ts
+++ src/app/api/staff/add/route.ts
@@ -1,6 +1,6 @@
 import { db } from "@/src/utils/api";
 
-export default async function POST(request: Request) {
+export async function POST(request: Request) {
   const { name, username, password, role } = await request.json();
 
   const staff = await db.user.create({
